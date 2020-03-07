27da181621caf0646a91486e10616f456ccd08d1
Pradnesh Khedekar <48099829+Pradnesh8@users.noreply.github.com>
2020-03-07 17:46:30 +0530

    NEW

---
+++ config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDi54pk3QmlFukR95XFTdZv4whSufAccGQ",
+    authDomain: "water-sensor-366c2.firebaseapp.com",
+    databaseURL: "https://water-sensor-366c2.firebaseio.com",
+    projectId: "water-sensor-366c2",
+    storageBucket: "water-sensor-366c2.appspot.com",
+    messagingSenderId: "672679392172",
+    appId: "1:672679392172:web:4238cbb44533824ff24009"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
